ba6a919046886312b7a2b85a662655d48737294e
Tommy Lim <tlim24@gmail.com>
2016-11-07 17:02:11 -0800

    JS and CSS changes

--- starter-code/main.js
+++ starter-code/main.js
@@ -2,3 +2,12 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+
+if(cardOne===cardTwo&&cardOne==="queen"){
+	alert("Match found! Two Queens!");
+}else if(cardOne===cardTwo&&cardOne==="king"){
+	alert("Match found! Two Kings!");
+} else{
+	alert("Sorry, try again!");
+}
+
